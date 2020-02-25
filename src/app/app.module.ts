ff25194b3eefb170fd6206ea97aad701b5239416
Carlos Mejia <mejiacm@nv.ccsd.net>
2020-02-25 12:51:45 -0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { MenuComponent } from './common/menu/menu.component';
+import { LoginComponent } from './common/login/login.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    LoginComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
